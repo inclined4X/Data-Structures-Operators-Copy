2d44827fb8344c4f17cd489b60a1a5207e27760b
inclined4X <jezemiahsam48@gmail.com>
2024-03-02 23:17:49 +0000

    changed variable name

--- script.js
+++ script.js
@@ -114,17 +114,17 @@ document.body.append(document.createElement("button"));
 document.querySelector("button").addEventListener("click", function () {
   const text = document.querySelector("textarea").value;
   const rows = text.split("\n");
-  // console.log(rows);
+  console.log(rows);
 
   for (const [i, row] of rows.entries()) {
     const [first, second] = row.toLowerCase().trim().split("_");
     console.log(row, first, second);
 
-    const output = `${first}${second.replace(
+    const outputCheck = `${first}${second.replace(
       second[0],
       second[0].toUpperCase()
     )}`;
-    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+    console.log(`${outputCheck.padEnd(20)}${"✅".repeat(i + 1)}`);
   }
 });
 
